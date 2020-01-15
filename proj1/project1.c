f9757b4950c386b8fe2aa9517d50b31fe94c1adf
enablex3 <ereyes@citadel.edu>
2020-01-15 18:14:32 -0500

    fix same line issue

--- proj1/project1.c
+++ proj1/project1.c
@@ -2,17 +2,22 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 
 #define MAX_LINE 80 // Max command length
 
 int main(void) {
 	// variables for command parsing
-	char* args;
+	char *args[MAX_LINE/2 + 1];
+	char *tok;
+	int k;
 	const char* delim = " ";
 	char buffer[MAX_LINE];
 
 	// run time variables
 	int should_run = 1;
+	int bg_run;
+	pid_t pid;
 
 	while (should_run) {
 		printf("project1>");
@@ -22,11 +27,29 @@ int main(void) {
 		// remove new line at the end
 		buffer[strcspn(buffer, "\n")] = 0;
 		// split input into args char array
-		args = strtok(buffer, delim);
-		/*while (args != NULL) {
-			printf("%s\n", args);
-			args = strtok(NULL, delim);
-		}*/
+		tok = strtok(buffer, delim);
+		k = 0;
+		while (tok != NULL) {
+			args[k] = tok;
+			if (tok == "&") {
+				bg_run = 1;
+			}
+			else {
+				bg_run = 0;
+			}
+			tok = strtok(NULL, delim);
+		}
+		pid = fork();
+		if (pid == 0) {
+			execvp(args[0], args);
+			return 0;
+		}
+		else if (pid > 0) {
+			if (bg_run) {
+				wait(NULL);
+			}
+		}
+		
 	}
 	return 0;
 }
