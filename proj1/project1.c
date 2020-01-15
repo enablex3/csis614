a7a8281486ffd307e69ce0e0f5e9ab261e89a879
enablex3 <ereyes@citadel.edu>
2020-01-15 08:25:13 -0500

    project 1 underway

---
+++ proj1/project1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE 80 // Max command length
+
+int main(void) {
+	// variables for command parsing
+	char* args;
+	const char* delim = " ";
+	char buffer[MAX_LINE];
+
+	// run time variables
+	int should_run = 1;
+
+	while (should_run) {
+		printf("project1>");
+		fflush(stdout);
+		// get input command line
+		fgets(buffer, sizeof(buffer), stdin);
+		// remove new line at the end
+		buffer[strcspn(buffer, "\n")] = 0;
+		args = strtok(buffer, delim);
+		while (args != NULL) {
+			printf("%s\n", args);
+			args = strtok(NULL, delim);
+		}
+	}
+	return 0;
+}
